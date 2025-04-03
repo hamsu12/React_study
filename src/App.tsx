7c916f85be1f90090a12d79ab1f5790f7d9fe36c
hamsu12 <hamsj3808@gmail.com>
2025-04-03 13:20:55 +0900

    WeatherApp 및 App.tsx 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+interface WelcomeProps{
+    name : string;
+}
+
+function Welcome(props:WelcomeProps){
+    return <h1>안녕하세요 {props.name}님</h1>;
+}
+
 function App() {
+    const name = "Hamsu"
+    const style={
+        color:"blue",
+        background:"yellow",
+    };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="header">
+      <h1 style={style}>Hello World</h1>
+        <p>저는 {name}입니다.</p>
+        <Greeting/>
+        <Welcome name="수정"/>
+        <Counter />
     </div>
   );
+}
+
+function Greeting(){
+    return <h1>안녕하세요!</h1>
+}
+
+function Counter(){
+    const [count, setCount] = useState(0);
+    return (
+        <div>
+            <p>현재 카운트 : {count}</p>
+            <button onClick={() => setCount(count+1)}>증가</button>
+        </div>
+    )
 }
 
 export default App;
